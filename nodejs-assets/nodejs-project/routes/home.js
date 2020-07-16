d043f39a7e74ea98afa401b5a6b6456b02b9a684
Archie Godfrey <archiegod@gmail.com>
2020-07-16 20:26:41 +0100

    fixed home route

--- nodejs-assets/nodejs-project/routes/home.js
+++ nodejs-assets/nodejs-project/routes/home.js
@@ -2,12 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const {token} = req.body;
-
-  if (!token) {
-    return res.sendStatus(401);
-  }
-
   return res.sendStatus(200);
 });
 
